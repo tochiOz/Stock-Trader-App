1868b5d29ab4ef6dfd4f7ca7d296c86809c5d4dd
babatezz <you@example.com>
2019-02-27 19:57:55 +0100

    Template Pack

---
+++ components/footer.vue
@@ -0,0 +1,10 @@
+<template>
+    <footer class="w-full flex pin-b  p-4 bg-grey-darkest text-teal h-auto">
+        <div class="w-1/2 sm:inline-block sm:mt-0 text-teal-lighter hover:text-white mr-4">
+           <p>Vue Training &copy; 2019</p> 
+        </div>
+        <div class="w-1/2 sm:inline-block sm:mt-0 text-teal-lighter hover:text-white mr-4 ">
+            <p class="float-right"> Created by: Baba_Tezz</p>
+        </div>
+    </footer>
+</template>
